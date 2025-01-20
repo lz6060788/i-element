d9a3edc6b7fb9237583f87014465a03482fcbf27
912525302@qq.com <912525302@qq.com>
2025-01-20 16:32:09 +0800

    fix: 补充缺失的unocss预设，造成部分包css样式冗余

--- packages/popper/vite.config.ts
+++ packages/popper/vite.config.ts
@@ -1,3 +1,7 @@
 import { generateVueConfig } from '../build/scripts';
 
-export default generateVueConfig();
+export default generateVueConfig({
+  presetIElementOptions: {
+    include: ['popper'],
+  },
+});
